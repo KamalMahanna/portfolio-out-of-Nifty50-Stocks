c1e319317d3661f7ec98b7db1000ebf05622346d
KamalMahanna <kamalkiranmahanna@gmail.com>
2023-06-23 17:22:13 +0530

    changing user agent

--- script_streamlit.py
+++ script_streamlit.py
@@ -32,7 +32,7 @@ with st.sidebar:
 
 def top_stocks(n=50):
     url = "https://www.nseindia.com/api/equity-stockIndices?index=NIFTY%2050"
-    
+    response_status=0
     while response_status!=200:
         user_agent =  random.choice(["Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
